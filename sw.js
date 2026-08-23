c9fe9c1dd7533d557a3e096672f1db388ee81893
fuugafuu <fuga2013703@gmail.com>
2026-08-23 10:35:58 +0900

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='pixel-forge-v1';const ASSETS=['/','/index.html','/style.css','/app.js','/manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}).catch(()=>caches.match('/index.html'))))});
